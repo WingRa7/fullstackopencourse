36a99745568e1a77bb745f3b1a485e9a01f19b8c
Tom Raine <tomraine@gmail.com>
2025-03-21 22:41:58 +0800

    Exercise 2.1 complete

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,61 @@
+const App = () => {
+
+  const Course = ({ course }) => { 
+
+    const Headers = () => {
+      return(
+        <h1>{course.name}</h1>
+        )
+      }
+    const Content = () => {
+      return(
+        <div>
+            {course.parts.map(part =>
+              <Part key={part.id} name={part.name} exercises={part.exercises}/>
+            )}
+        </div>
+      )
+     }
+    const Part = ({ name, exercises}) => {
+     return(
+     <>
+     <p>{name} {exercises}</p>
+     </>
+      )
+    }
+    
+    return (
+      <div>
+        <Headers  />
+        <Content  />
+      </div>
+    )
+  }
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+export default App
