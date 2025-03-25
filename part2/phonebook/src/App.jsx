65385f74e45348c7ee2f3f17225d8eddb8e5caed
Tom Raine <tomraine@gmail.com>
2025-03-25 16:01:19 +0800

    Exercise 2.9 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,18 +6,16 @@ const App = ({ directory }) => {
   const [persons, setPersons] = useState(directory) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const addName = (event) => {
-    
     event.preventDefault()
     const nameObject = {
       name: newName,
       number: newNumber
     }
-
     const checkDuplicateName = (person) => person.name.toLowerCase() === newName.toLowerCase()
     const duplicateName = persons.some(checkDuplicateName)
-
     if (duplicateName === false) {
     setPersons(persons.concat(nameObject))
     setNewName('')
@@ -28,26 +26,31 @@ const App = ({ directory }) => {
       alert(`${newName} is already in the phonebook`)
     }
   }
-  
+
+  const searchResult = persons.filter((person) => person.name.toLowerCase().startsWith(search.toLowerCase()))
+
   const handleNameEntryChange = (event) => {
     setNewName(event.target.value)
   }
   const handleNumberEntryChange = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <div> filter shown with <input value={search} onChange={handleSearchChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div> name: <input value={newName} onChange={handleNameEntryChange}/></div>
         <div> number: <input value={newNumber} onChange={handleNumberEntryChange}/></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-        {persons.map((entry) => (
+        {searchResult.map((entry) => (
           <Entry key={entry.name} entry={entry} />
         ))}
       </div>
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 const directory = [
-  { name: 'Arto Hellas', number: 66442277 }
+  { name: 'Carlos Whispers', number: 66442277, id: 1 },
+  { name: 'Gary Chapati', number: 83864949, id: 2 },
+  { name: 'Gerhardt Nightcreeper', number: 77720402, id: 3 },
+  { name: 'Chad Salad', number: 80848084, id: 4 },
 ]
 
 createRoot(document.getElementById('root')).render(
