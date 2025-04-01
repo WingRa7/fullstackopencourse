3f95688cb36934574f3be824045a6a6b8c8fa512
Tom Raine <tomraine@gmail.com>
2025-04-01 15:08:48 +0800

    Exercise 2.19 complete

--- part2/countries/src/Components/Country.jsx
+++ part2/countries/src/Components/Country.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react'
+
 const Country = ({ country, resultLength }) => {
 
-    console.log(country)
+    const [toggle, setToggle] = useState(true)
+    const toggleChange = () => {
+        setToggle((newToggle) => !newToggle)
+    }
+
+    const DropdownButton = () => <button onClick={toggleChange} toggle={toggle}>{toggle ? 'Show' : 'Hide'}</button>
+
+    const CountryProfile = (props) => {
+        console.log('country profile', props)
+        return(
+            <>
+            <h1>{country.name.common}</h1>
+            <p>Capital {country.capital}<br/>Area {country.area}</p>
+            <h2>Languages</h2>
+            <ul>{Object.values(country.languages).map(language => <li key={language}>{language}</li>)}</ul>
+            <img src={country.flags.png} />
+            </>
+        )
+}
+    
     return(
         <>
         {(resultLength == 1) 
         ?    /* single country display */
-         <>
-        <h1>{country.name.common}</h1>
-        <p>Capital {country.capital}<br/>Area {country.area}</p>
-        <h2>Languages</h2>
-        <ul>{Object.values(country.languages).map(language => <li key={language}>{language}</li>)}</ul>
-        <img src={country.flags.png} />
-        </>
-
+            <CountryProfile />
            
         :    /* country list display */
-         <>{country.name.common}<br/></> 
+         <>
+         {country.name.common} <DropdownButton /><br/>
+         {toggle ? null : <><CountryProfile /><br/><br/></>}
+         </>
+
         }    
         </>
     )
