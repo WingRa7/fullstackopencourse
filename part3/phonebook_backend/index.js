1b08b99d1203610ff00fd4d8462e36304ee18230
Tom Raine <tomraine@gmail.com>
2025-04-04 15:31:12 +0800

    Exercise 3.3 complete

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -37,6 +37,17 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+  app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const note = persons.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
