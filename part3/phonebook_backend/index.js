22ead1f17dbdbf5c3adb8f336d1ae2a1a6c82e13
Tom Raine <tomraine@gmail.com>
2025-04-05 13:13:31 +0800

    Exercise 3.7 complete

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
-app.use(express.json())
 
 let persons = [
         { 
@@ -25,6 +25,9 @@ let persons = [
           "number": "39-23-6423122"
         }
     ]
+
+app.use(express.json())
+app.use(morgan('tiny'))
 
 const generateRandomId = (max) => {
    return String(Math.floor(Math.random() * max))
