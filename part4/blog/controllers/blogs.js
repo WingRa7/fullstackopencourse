4c63b992ffd9126c64058ff3fa0d06d42d6a554c
Tom Raine <tomraine@gmail.com>
2025-04-14 17:06:30 +0800

    Added error handling to .post route

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -10,9 +10,11 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
   
-    blog.save().then((result) => {
+    blog.save()
+      .then((result) => {
       response.status(201).json(result)
     })
+      .catch(error => next(error))
   })
 
 module.exports = blogsRouter
