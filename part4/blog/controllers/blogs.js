00b9344d09deb37f17d7f6ea673220413c3db2f8
Tom Raine <tomraine@gmail.com>
2025-05-04 13:56:00 +0800

    Exercise 4.17 complete

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -20,19 +20,22 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  // const user = await User.findById(body.userId)
+
+  const randomUser = await User.findOne() // random user replaces user
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user._id
+    user: randomUser._id //user._id
   })
 
   const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  randomUser.blogs = randomUser.blogs.concat(savedBlog._id) // concat blogs to random user
+  // user.blogs = user.blogs.concat(savedBlog._id)
+  await randomUser.save() //await user.save()
 
   response.status(201).json(savedBlog)
 })
