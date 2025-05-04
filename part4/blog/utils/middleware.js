852cd649c4ec9cae6246f1b1f5c6eb0b878e0cf2
Tom Raine <tomraine@gmail.com>
2025-05-04 23:26:37 +0800

    Exercises 4.19 & 4.20 complete

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -4,6 +4,7 @@ const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
+  logger.info('Token:', request.token)
   logger.info('---')
   next()
 }
@@ -29,12 +30,20 @@ const errorHandler = (error, request, response, next) => {
     })
   }
 
-
   next(error)
 }
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  request.token = authorization
+  next()
+}
+
+
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
