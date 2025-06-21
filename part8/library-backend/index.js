8d60821dc0944f41d13c5e9bd6a66112c2be51de
Tom Raine <tomgraine@gmail.com>
2025-06-21 15:07:48 +0800

    Exercise 8.4 complete

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -105,8 +105,8 @@ type Author {
 type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
-    allAuthors: [Author!]!
+    allBooks(author: String!): [Book!]!
+    allAuthors: [Author]!
   }
 `;
 
@@ -114,7 +114,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () => authors,
   },
   Author: {
