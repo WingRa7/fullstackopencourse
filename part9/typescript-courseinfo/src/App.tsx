f038354d65636675a44f738912bde940d3a36a23
Tom Raine <tomgraine@gmail.com>
2025-09-12 18:08:15 +0800

    Exercise 9.16 in progress

--- part9/typescript-courseinfo/src/App.tsx
+++ part9/typescript-courseinfo/src/App.tsx
@@ -1,23 +1,127 @@
 const App = () => {
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  }
+
+  interface CoursePartBasePlus extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartBasic extends CoursePartBasePlus {
+    kind: "basic";
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group";
+  }
+
+  interface CoursePartBackground extends CoursePartBasePlus {
+    backgroundMaterial: string;
+    kind: "background";
+  }
+
+  interface CoursePartSpecial extends CoursePartBasePlus {
+    requirements: string[];
+    kind: "special";
+  }
+
+  type CoursePart =
+    | CoursePartBasic
+    | CoursePartGroup
+    | CoursePartBackground
+    | Special;
+
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
   const totalExercises = courseParts.reduce(
     (sum, part) => sum + part.exerciseCount,
     0
   );
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  interface PartProps {
+    part: CoursePart;
+  }
+
+  const Part = ({ part }: PartProps) => {
+    switch (part.kind) {
+      case "basic":
+        return (
+          <p>
+            {part.name} {part.exerciseCount} {part.description}
+          </p>
+        );
+      case "group":
+        return (
+          <p>
+            {part.name} {part.exerciseCount} {part.groupProjectCount}
+          </p>
+        );
+      case "background":
+        return (
+          <p>
+            {part.name} {part.exerciseCount} {part.description}
+            {part.backgroundMaterial}
+          </p>
+        );
+      case "special":
+        return (
+          <p>
+            {part.name} {part.exerciseCount} {part.description}
+            {part.requirements}
+          </p>
+        );
+      default:
+        return assertNever(part);
+    }
+  };
 
   interface HeaderProps {
     courseName: string;
@@ -26,37 +130,29 @@ const App = () => {
     return <h1>{props.courseName}</h1>;
   };
 
-  interface CoursePart {
-    name: string;
-    exerciseCount: number;
-  }
-
   interface ContentProps {
     parts: CoursePart[];
   }
+
   const Content = (props: ContentProps) => {
     return props.parts.map((part) => {
-      return (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
-      );
+      return <Part part={part} />;
     });
   };
 
-  const Total = (props: ContentProps) => {
-    const total = props.parts.reduce(
-      (acc, value) => acc + value.exerciseCount,
-      0
-    );
-    return <p>Number of exercises {total}</p>;
+  interface TotalProps {
+    total: number;
+  }
+
+  const Total = (props: TotalProps) => {
+    return <p>Number of exercises {props.total}</p>;
   };
 
   return (
     <div>
       <Header courseName={courseName} />
       <Content parts={courseParts} />
-      <Total parts={courseParts} />
+      <Total total={totalExercises} />
     </div>
   );
 };
