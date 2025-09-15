cd8c68ddbf6f91c2f7d58485c13fb679c4a5845e
Tom Raine <tomgraine@gmail.com>
2025-09-16 00:22:30 +0800

    Exercise 9.20 complete

--- part9/typescript-flights/frontend/src/App.tsx
+++ part9/typescript-flights/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import { getAllDiaryEntries, createDiary } from "./services/diaryServices";
-import type { DiaryEntry } from "./types";
+import { type DiaryEntry, Weather, Visibility } from "./types";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
   const [newDate, setNewDate] = useState("");
-  const [newWeather, setNewWeather] = useState("");
-  const [newVisibility, setNewVisibility] = useState("");
+  const [newWeather, setNewWeather] = useState<Weather>(Weather.Sunny);
+  const [newVisibility, setNewVisibility] = useState<Visibility>(
+    Visibility.Great
+  );
   const [newComment, setNewComment] = useState("");
   const [notify, setNotify] = useState<string | null>(null);
 
@@ -28,8 +30,8 @@ const App = () => {
       });
       setDiaries(diaries.concat(response));
       setNewDate("");
-      setNewWeather("");
-      setNewVisibility("");
+      setNewWeather(Weather.Sunny);
+      setNewVisibility(Visibility.Good);
       setNewComment("");
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -50,23 +52,92 @@ const App = () => {
         <div>
           date
           <input
+            type="date"
             value={newDate}
             onChange={(event) => setNewDate(event.target.value)}
           />
         </div>
         <div>
-          weather
-          <input
-            value={newWeather}
-            onChange={(event) => setNewWeather(event.target.value)}
-          />
+          <div>
+            weather
+            <input
+              type="radio"
+              name="weather"
+              value="sunny"
+              id="sunny"
+              onChange={() => setNewWeather(Weather.Sunny)}
+            />
+            <label htmlFor="sunny">Sunny</label>
+            <input
+              type="radio"
+              name="weather"
+              value="rainy"
+              id="rainy"
+              onChange={() => setNewWeather(Weather.Rainy)}
+            />
+            <label htmlFor="rainy">Rainy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="cloudy"
+              id="cloudy"
+              onChange={() => setNewWeather(Weather.Cloudy)}
+            />
+            <label htmlFor="cloudy">Cloudy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="stormy"
+              id="stormy"
+              onChange={() => setNewWeather(Weather.Stormy)}
+            />
+            <label htmlFor="Stormy">Stormy</label>
+            <input
+              type="radio"
+              name="weather"
+              value="windy"
+              id="windy"
+              onChange={() => setNewWeather(Weather.Windy)}
+            />
+            <label htmlFor="Windy">Windy</label>
+          </div>
         </div>
         <div>
-          visibility
-          <input
-            value={newVisibility}
-            onChange={(event) => setNewVisibility(event.target.value)}
-          />
+          <div>
+            visibility
+            <input
+              type="radio"
+              name="visibility"
+              value="great"
+              id="great"
+              onChange={() => setNewVisibility(Visibility.Great)}
+            />
+            <label htmlFor="great">Great</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="good"
+              id="good"
+              onChange={() => setNewVisibility(Visibility.Good)}
+            />
+            <label htmlFor="good">Good</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="ok"
+              id="ok"
+              onChange={() => setNewVisibility(Visibility.Ok)}
+            />
+            <label htmlFor="ok">Ok</label>
+            <input
+              type="radio"
+              name="visibility"
+              value="poor"
+              id="poor"
+              onChange={() => setNewVisibility(Visibility.Poor)}
+            />
+            <label htmlFor="poor">Poor</label>
+          </div>
         </div>
         <div>
           comment
